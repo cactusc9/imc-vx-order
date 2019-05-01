2693279d0705884c9c9e7dc4a1a7fdc0a5db3e73
cactusc9 <cactusc9@qq.com>
2019-05-01 19:15:54 +0800

    完成商品类别和详情的获取and test

---
+++ product/src/main/java/com/cactusc9/product/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.cactusc9.product.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+
+    /**
+     * 1. 查询所有在架商品
+     * 2. 查询类目type列表
+     * 3. 查询类目
+     * 4. 构造数据
+     * */
+    public void list(){
+
+    }
+}

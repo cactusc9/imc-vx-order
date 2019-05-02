39bd7d1914e42800e31aa69f9352c70c417df431
cactusc9 <cactusc9@qq.com>
2019-05-03 01:17:05 +0800

    product测试服务显示端口

--- product/src/main/java/com/cactusc9/product/controller/ServerController.java
+++ product/src/main/java/com/cactusc9/product/controller/ServerController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/server")
 public class ServerController {
 
     @GetMapping("/msg")
-    public String getMsg(){
-        return "this is product server!";
+    public String getMsg(HttpServletRequest request){
+        return "this is product server! port:"+request.getLocalPort();
     }
 
 }
